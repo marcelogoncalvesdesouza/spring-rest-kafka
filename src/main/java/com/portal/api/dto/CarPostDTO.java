6ebdd9ca0bc74f47385edb5bcc874649bd12c305
Marcelo Gonçalves de Souza <marcelo.souza@dnweb.com.br>
2023-05-18 22:17:34 -0300

    Adjustments and tests

--- src/main/java/com/portal/api/dto/CarPostDTO.java
+++ src/main/java/com/portal/api/dto/CarPostDTO.java
@@ -15,11 +15,11 @@ public class CarPostDTO {
 
     private String model;
     private String brand;
-    private String price;
+    private Double price;
     private String description;
     private String engineVersion;
     private String city;
-    private String createDate;
+    private String createdDate;
     private String ownerId;
     private String ownerName;
     private String ownerType;
